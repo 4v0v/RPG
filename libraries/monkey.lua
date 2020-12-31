b24cb2636d13a444fc44853e9ad20f92ffa90309
4v0v <30574115+4v0v@users.noreply.github.com>
2020-12-31 19:59:27 +0100

    monkey

--- libraries/monkey.lua
+++ libraries/monkey.lua
@@ -1,35 +1,42 @@
-table.insert(package.loaders, 2, function(name)
-	local name = name:gsub("%.", "/") .. ".lua"
-	local file = love.filesystem.read(name)
-	if not file then return end
-
-	local number = "([%d]+)"
-	local var = "([%w%.:_%[%]'\"]+)"
+local function parse(s)
+	local str = s
+	local integer    = "([%d]+)"
 	local simple_var = "([%w_]+)"
-	local var_with_parenthesis = "([%w%.:_%[%]'\"%(%)]+)"
+	local var        = [=[([%w%.:_'"%[%]]+)]=]
+	local expression = [[([%w%.:_'"%[%]%(%)]+)]]
+	local wl         = "[%s]+"
+	local _wl        = "[%s]?"
 
 	local patterns = {
-		{ pattern = "%-%-[^\n]+"     , replacement = ""}, -- remove comments
-		{ pattern = "@"              , replacement = "self"},
-		{ pattern = var .. "%s*%+="  , replacement = "%1 = %1 + "}, -- +=
-		{ pattern = var .. "%s*%-="  , replacement = "%1 = %1 - "}, -- -=
-		{ pattern = var .. "%s*%*="  , replacement = "%1 = %1 * "}, -- *=
-		{ pattern = var .. "%s*/="   , replacement = "%1 = %1 / "}, -- /=
-		{ pattern = var .. "%s*^="   , replacement = "%1 = %1 ^ "}, -- ^=
-		{ pattern = var .. "%s*%%="  , replacement = "%1 = %1 %% "}, -- %=
-		{ pattern = var .. "%s*%.%.=", replacement = "%1 = %1 .. "}, -- ..=
-		{ pattern = var .. "%s*%+%+" , replacement = "%1 = %1 + 1"}, -- ++
-		{ pattern = "&&"             , replacement = " and "},
-		{ pattern = "||"             , replacement = " or "},
-		{ pattern = "!="             , replacement = "~="},
-		{ pattern = "!" .. var_with_parenthesis, replacement = " not %1"},
-		{ pattern = "([%s,={%(])fn%(", replacement = "%1function("}, -- fn() end
-		{ pattern = "for[%s]+".. number .. "[%s]+do", replacement = "for it = 1, %1 do"}, -- for 100 do end
-		{ pattern = "for[%s]+".. var_with_parenthesis .. "[%s]+do", replacement = "for key, it in pairs(%1) do"}, -- for table do end
-		{ pattern = "for[%s]+".. simple_var .. "[%s]+in[%s]+" .. var .. "[%s]+do", replacement = "for key, %1 in pairs(%2) do"}, -- for value in table do end
-		{ pattern = "for[%s]+".. simple_var .. ",[%s]?" .. simple_var .. "[%s]+in[%s]+" .. var .. "[%s]+do", replacement = "for %1, %2 in pairs(%3) do"}, -- for key, value in table do end
+		{ patt = "%-%-[^\n]+"         , repl = ""},
+		{ patt = "@"                  , repl = "self"},
+		{ patt = var .. _wl .. "%+="  , repl = "%1 = %1 + "},
+		{ patt = var .. _wl .. "%-="  , repl = "%1 = %1 - "},
+		{ patt = var .. _wl .. "%*="  , repl = "%1 = %1 * "},
+		{ patt = var .. _wl .. "/="   , repl = "%1 = %1 / "},
+		{ patt = var .. _wl .. "^="   , repl = "%1 = %1 ^ "},
+		{ patt = var .. _wl .. "%%="  , repl = "%1 = %1 %% "},
+		{ patt = var .. _wl .. "%.%.=", repl = "%1 = %1 .. "},
+		{ patt = var .. _wl .. "%+%+" , repl = "%1 = %1 + 1"},
+		{ patt = "&&"                 , repl = " and "},
+		{ patt = "||"                 , repl = " or "},
+		{ patt = "!="                 , repl = " ~= "},
+		{ patt = "!"                  , repl = " not "},
+		{ patt = "fn%("               , repl = "function("},
+		{ patt = "for" .. wl .. integer .. wl .. "do", repl = "for it = 1, %1 do"},
+		{ patt = "for" .. wl .. expression .. wl .. "do", repl = "for key, it in pairs(%1) do"},
+		{ patt = "for" .. wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for key, %1 in pairs(%2) do"},
+		{ patt = "for" .. wl .. simple_var .. "," .. _wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for %1, %2 in pairs(%3) do"},
 	}
 
-	for i, v in ipairs(patterns) do file = file:gsub(v.pattern, v.replacement) end
-	return assert(loadstring(file))
+	for i, v in ipairs(patterns) do str = str:gsub(v.patt, v.repl) end
+	return str
+end
+
+table.insert(package.loaders, 2, function(name)
+	local name        = name:gsub("%.", "/") .. ".lua"
+	local file        = love.filesystem.read(name)
+	local parsed_file = parse(file)
+
+	return assert(loadstring(parsed_file))
 end)
