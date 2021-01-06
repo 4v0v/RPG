34507e765f2eb1cc58391977472d011a80abca0c
4v0v <30574115+4v0v@users.noreply.github.com>
2021-01-06 02:12:51 +0100

    ...

--- libraries/monkey.lua
+++ libraries/monkey.lua
@@ -25,11 +25,14 @@ local function parse(s)
 		{ patt = "fn%("               , repl = "function("},
 		{ patt = "for" .. wl .. integer .. wl .. "do", repl = "for it = 1, %1 do"},
 		{ patt = "for" .. wl .. expression .. wl .. "do", repl = "for key, it in pairs(%1) do"},
+		{ patt = "ifor" .. wl .. expression .. wl .. "do", repl = "for key, it in ipairs(%1) do"},
 		{ patt = "for" .. wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for key, %1 in pairs(%2) do"},
+		{ patt = "ifor" .. wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for key, %1 in ipairs(%2) do"},
 		{ patt = "for" .. wl .. simple_var .. "," .. _wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for %1, %2 in pairs(%3) do"},
+		{ patt = "ifor" .. wl .. simple_var .. "," .. _wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for %1, %2 in ipairs(%3) do"},
 	}
 
-	for i, v in ipairs(patterns) do str = str:gsub(v.patt, v.repl) end
+	for _, v in ipairs(patterns) do str = str:gsub(v.patt, v.repl) end
 	return str
 end
 
